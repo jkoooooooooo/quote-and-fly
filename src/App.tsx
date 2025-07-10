2b704d9b2467c5f6519099560531083ffcbbd10b
jkoooooooooo <kacharinisthu@gmail.com>
2025-07-10 16:15:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Index from './pages/Index'; // or './components/Index'
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
